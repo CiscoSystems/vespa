05f0275123ad3378b94edaccddef40d1e721a8ec
Arvind Somya <asomya@cisco.com>
2013-10-02 12:41:55 -0700

    Adding an IFC manager around tenant creation

--- neutron/plugins/ml2/drivers/vespa/mechanism_vespa.py
+++ neutron/plugins/ml2/drivers/vespa/mechanism_vespa.py
@@ -12,14 +12,60 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
+# @author: Arvind Somya (asomya@cisco.com), Cisco Systems Inc.
 
 from oslo.config import cfg
 
 from neutron.openstack.common import log
 from neutron.plugins.ml2 import driver_api as api
 
+
 LOG = log.getLogger(__name__)
+
+
+class IFCManager(object):
+    def __init__(self):
+        # Update list of tenants from the IFC
+        self._get_ifc_tenant_list()
+
+    def _get_ifc_tenant_list(self):
+        """Get a list of all tenants from the IFC."""
+        pass
+
+    def _is_tenant_created_on_ifc(self, tenant_id, tenant_name=None):
+        """Check if a tenant exists on the IFC.
+        
+        Check the local tenant cache and create a new tenant 
+        if not found
+        """
+        pass
+
+    def _create_tenant_on_ifc(self, tenant_id, tenant_name):
+        """Create an Openstack tenant on the IFC."""
+        pass
+
 
 class VespaMechanismDriver(api.MechanismDriver):
     def initialize(self):
+        self.ifc_manager = IFCManager()
+
+    def create_port_precommit(self, context):
+        # Get tenant details from port context
+        tenant_id = port['tenant_id']
+        self.ifc_manager._is_tenant_created_on_ifc(tenant_id)
+
+    def create_port_postcommit(self, context):
+        pass
+
+    def create_network_precommit(self, context):
+        pass
+
+    def create_network_postcommit(self, context):
+        pass
+
+    def create_subnet_precommit(self, context):
+        pass
+
+    def create_subnet_postcommit(self, context):
         pass
